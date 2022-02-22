69e2bf1acfcf04cc87ba26a30b8a2c5cbab8d7ce
Adrian Aneci <aneci@adobe.com>
2022-02-22 13:32:45 +0200

    Add azure groups support and oauth2 v2.0

--- providers/azure.go
+++ providers/azure.go
@@ -3,30 +3,37 @@ package providers
 import (
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/confidential"
 	"github.com/bitly/go-simplejson"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/options"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/sessions"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/logger"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/requests"
+	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/util"
 )
 
 // AzureProvider represents an Azure based Identity Provider
 type AzureProvider struct {
 	*ProviderData
-	Tenant string
+	Tenant                             string
+	GroupField                         string
+	isV2Endpoint                       bool
+	MicrosoftGraphAccessToken          string
+	MicrosoftGraphAccessTokenExpiresOn *time.Time
 }
 
 var _ Provider = (*AzureProvider)(nil)
 
 const (
-	azureProviderName = "Azure"
-	azureDefaultScope = "openid"
+	azureProviderName      = "Azure"
+	azureDefaultScope      = "openid"
+	azureDefaultGroupField = "id"
 )
 
 var (
@@ -56,9 +63,10 @@ var (
 
 	// Default ProtectedResource URL for Azure.
 	// Pre-parsed URL of https://graph.microsoft.com.
-	azureDefaultProtectResourceURL = &url.URL{
+	azureGraphURL = &url.URL{
 		Scheme: "https",
 		Host:   "graph.microsoft.com",
+		Path:   ".default",
 	}
 )
 
@@ -73,9 +81,6 @@ func NewAzureProvider(p *ProviderData, opts options.AzureOptions) *AzureProvider
 		scope:       azureDefaultScope,
 	})
 
-	if p.ProtectedResource == nil || p.ProtectedResource.String() == "" {
-		p.ProtectedResource = azureDefaultProtectResourceURL
-	}
 	if p.ValidateURL == nil || p.ValidateURL.String() == "" {
 		p.ValidateURL = p.ProfileURL
 	}
@@ -88,9 +93,21 @@ func NewAzureProvider(p *ProviderData, opts options.AzureOptions) *AzureProvider
 		overrideTenantURL(p.RedeemURL, azureDefaultRedeemURL, tenant, "token")
 	}
 
+	groupField := azureDefaultGroupField
+	if opts.GroupField != "" {
+		groupField = opts.GroupField
+	}
+
+	isV2Endpoint := false
+	if strings.Contains(p.LoginURL.String(), "v2.0") {
+		isV2Endpoint = true
+	}
+
 	return &AzureProvider{
 		ProviderData: p,
 		Tenant:       tenant,
+		GroupField:   groupField,
+		isV2Endpoint: isV2Endpoint,
 	}
 }
 
@@ -103,9 +120,31 @@ func overrideTenantURL(current, defaultURL *url.URL, tenant, path string) {
 	}
 }
 
+func getMicrosoftGraphURL(user string) *url.URL {
+	return &url.URL{
+		Scheme:   "https",
+		Host:     "graph.microsoft.com",
+		Path:     "/v1.0/users/" + user + "/transitiveMemberOf",
+		RawQuery: "$select=displayName,id",
+	}
+}
+
 func (p *AzureProvider) GetLoginURL(redirectURI, state, _ string, extraParams url.Values) string {
+	// In azure oauth v2 there is no resource param
+	// https://docs.microsoft.com/en-us/azure/active-directory/azuread-dev/azure-ad-endpoint-comparison#scopes-not-resources
+	if p.isV2Endpoint {
+		// In azure oauth v2 there is no groups scope so replace it if exists
+		p.Scope = strings.ReplaceAll(p.Scope, " groups", "")
+	}
 	if p.ProtectedResource != nil && p.ProtectedResource.String() != "" {
-		extraParams.Add("resource", p.ProtectedResource.String())
+		if p.isV2Endpoint {
+			if !strings.Contains(p.Scope, p.ProtectedResource.String()) {
+				// protected resource if configured will be added as scope
+				p.Scope += " " + p.ProtectedResource.String()
+			}
+		} else {
+			extraParams.Add("resource", p.ProtectedResource.String())
+		}
 	}
 	a := makeLoginURL(p.ProviderData, redirectURI, state, extraParams)
 	return a.String()
@@ -145,45 +184,39 @@ func (p *AzureProvider) Redeem(ctx context.Context, redirectURL, code, codeVerif
 	session.CreatedAtNow()
 	session.SetExpiresOn(time.Unix(jsonResponse.ExpiresOn, 0))
 
-	email, err := p.verifyTokenAndExtractEmail(ctx, session.IDToken, session.AccessToken)
+	err = p.extractClaimsIntoSession(ctx, session)
 
-	// https://github.com/oauth2-proxy/oauth2-proxy/pull/914#issuecomment-782285814
-	// https://github.com/AzureAD/azure-activedirectory-library-for-java/issues/117
-	// due to above issues, id_token may not be signed by AAD
-	// in that case, we will fallback to access token
-	if err == nil && email != "" {
-		session.Email = email
-	} else {
-		logger.Printf("unable to get email claim from id_token: %v", err)
+	if err != nil {
+		logger.Printf("unable to get email and/or groups claims from token: %v", err)
+	}
+
+	return session, err
+}
+
+// EnrichSession enriches the session state with userID, mail and groups
+func (p *AzureProvider) EnrichSession(ctx context.Context, session *sessions.SessionState) error {
+	err := p.extractClaimsIntoSession(ctx, session)
+
+	if err != nil {
+		logger.Printf("unable to get email and/or groups claims from token: %v", err)
 	}
 
 	if session.Email == "" {
-		email, err = p.verifyTokenAndExtractEmail(ctx, session.AccessToken, session.AccessToken)
-		if err == nil && email != "" {
-			session.Email = email
-		} else {
-			logger.Printf("unable to get email claim from access token: %v", err)
+		email, err := p.getEmailFromProfileAPI(ctx, session.AccessToken)
+		if err != nil {
+			return fmt.Errorf("unable to get email address from profile URL: %v", err)
 		}
+		session.Email = email
 	}
 
-	return session, nil
-}
-
-// EnrichSession finds the email to enrich the session state
-func (p *AzureProvider) EnrichSession(ctx context.Context, s *sessions.SessionState) error {
-	if s.Email != "" {
-		return nil
+	// If using the v2.0 oidc endpoint we're also querying Microsoft Graph
+	if p.isV2Endpoint {
+		groups, err := p.getGroupsFromMicrosoftGraphAPI(ctx, session)
+		if err != nil {
+			return fmt.Errorf("unable to get groups from Microsoft Graph: %v", err)
+		}
+		session.Groups = util.RemoveDuplicateStr(append(session.Groups, groups...))
 	}
-
-	email, err := p.getEmailFromProfileAPI(ctx, s.AccessToken)
-	if err != nil {
-		return fmt.Errorf("unable to get email address: %v", err)
-	}
-	if email == "" {
-		return errors.New("unable to get email address")
-	}
-	s.Email = email
-
 	return nil
 }
 
@@ -205,33 +238,76 @@ func (p *AzureProvider) prepareRedeem(redirectURL, code, codeVerifier string) (u
 	if codeVerifier != "" {
 		params.Add("code_verifier", codeVerifier)
 	}
-	if p.ProtectedResource != nil && p.ProtectedResource.String() != "" {
-		params.Add("resource", p.ProtectedResource.String())
+
+	// In azure oauth v2 there is no resource param
+	// https://docs.microsoft.com/en-us/azure/active-directory/azuread-dev/azure-ad-endpoint-comparison#scopes-not-resources
+	if p.isV2Endpoint {
+		// In azure oauth v2 there is no groups scope so replace it if exists
+		p.Scope = strings.ReplaceAll(p.Scope, " groups", "")
 	}
-	return params, nil
-}
 
-// verifyTokenAndExtractEmail tries to extract email claim from either id_token or access token
-// when oidc verifier is configured
-func (p *AzureProvider) verifyTokenAndExtractEmail(ctx context.Context, rawIDToken string, accessToken string) (string, error) {
-	email := ""
-
-	if rawIDToken != "" && p.Verifier != nil {
-		_, err := p.Verifier.Verify(ctx, rawIDToken)
-		// due to issues mentioned above, id_token may not be signed by AAD
-		if err == nil {
-			s, err := p.buildSessionFromClaims(rawIDToken, accessToken)
-			if err == nil {
-				email = s.Email
-			} else {
-				logger.Printf("unable to get claims from token: %v", err)
+	if p.ProtectedResource != nil && p.ProtectedResource.String() != "" {
+		if p.isV2Endpoint {
+			if !strings.Contains(p.Scope, p.ProtectedResource.String()) {
+				// protected resource if configured will be added as scope
+				p.Scope += " " + p.ProtectedResource.String()
 			}
 		} else {
-			logger.Printf("unable to verify token: %v", err)
+			params.Add("resource", p.ProtectedResource.String())
 		}
 	}
 
-	return email, nil
+	return params, nil
+}
+
+// extractClaimsIntoSession tries to extract email and groups claims from either id_token or access token
+// when oidc verifier is configured
+func (p *AzureProvider) extractClaimsIntoSession(ctx context.Context, session *sessions.SessionState) error {
+
+	var innerErr error
+	var s *sessions.SessionState
+
+	// First let's verify session token
+	err := p.verifySessionToken(ctx, session)
+	if err == nil {
+		// https://github.com/oauth2-proxy/oauth2-proxy/pull/914#issuecomment-782285814
+		// https://github.com/AzureAD/azure-activedirectory-library-for-java/issues/117
+		// due to above issues, id_token may not be signed by AAD
+		// in that case, we will fallback to access token
+		s, innerErr = p.buildSessionFromClaims(session.IDToken, session.AccessToken)
+		if innerErr != nil || s.Email == "" {
+			s, innerErr = p.buildSessionFromClaims(session.AccessToken, session.AccessToken)
+		}
+
+		if innerErr != nil {
+			err = fmt.Errorf("unable to get claims from token: %v", innerErr)
+		} else {
+			session.Email = s.Email
+			if s.Groups != nil {
+				session.Groups = s.Groups
+			}
+		}
+	} else {
+		err = fmt.Errorf("unable to verify token: %v", err)
+	}
+
+	return err
+}
+
+// verifySessionToken tries to validate id_token if present or access token when oidc verifier is configured
+func (p *AzureProvider) verifySessionToken(ctx context.Context, session *sessions.SessionState) error {
+	var err error = nil
+	if p.Verifier != nil {
+		if session.IDToken != "" {
+			if _, err = p.Verifier.Verify(ctx, session.IDToken); err != nil {
+				logger.Printf("unable to verify ID token, fallback to access token: %v", err)
+				_, err = p.Verifier.Verify(ctx, session.AccessToken)
+			}
+			return err
+		}
+		_, err = p.Verifier.Verify(ctx, session.AccessToken)
+	}
+	return err
 }
 
 // RefreshSession uses the RefreshToken to fetch new Access and ID Tokens
@@ -285,25 +361,10 @@ func (p *AzureProvider) redeemRefreshToken(ctx context.Context, s *sessions.Sess
 	s.CreatedAtNow()
 	s.SetExpiresOn(time.Unix(jsonResponse.ExpiresOn, 0))
 
-	email, err := p.verifyTokenAndExtractEmail(ctx, s.IDToken, s.AccessToken)
+	err = p.extractClaimsIntoSession(ctx, s)
 
-	// https://github.com/oauth2-proxy/oauth2-proxy/pull/914#issuecomment-782285814
-	// https://github.com/AzureAD/azure-activedirectory-library-for-java/issues/117
-	// due to above issues, id_token may not be signed by AAD
-	// in that case, we will fallback to access token
-	if err == nil && email != "" {
-		s.Email = email
-	} else {
-		logger.Printf("unable to get email claim from id_token: %v", err)
-	}
-
-	if s.Email == "" {
-		email, err = p.verifyTokenAndExtractEmail(ctx, s.AccessToken, s.AccessToken)
-		if err == nil && email != "" {
-			s.Email = email
-		} else {
-			logger.Printf("unable to get email claim from access token: %v", err)
-		}
+	if err != nil {
+		logger.Printf("unable to get email and/or groups claims from token: %v", err)
 	}
 
 	return nil
@@ -311,6 +372,104 @@ func (p *AzureProvider) redeemRefreshToken(ctx context.Context, s *sessions.Sess
 
 func makeAzureHeader(accessToken string) http.Header {
 	return makeAuthorizationHeader(tokenTypeBearer, accessToken, nil)
+}
+
+func (p *AzureProvider) acquireMicrosoftGraphToken(ctx context.Context) error {
+
+	cred, err := confidential.NewCredFromSecret(p.ClientSecret)
+	if err != nil {
+		return err
+	}
+	app, err := confidential.New(p.ClientID, cred, confidential.WithAuthority(p.LoginURL.String()))
+	if err != nil {
+		return err
+	}
+
+	var result confidential.AuthResult
+
+	if p.MicrosoftGraphAccessToken == "" {
+		result, err = app.AcquireTokenByCredential(ctx, []string{"openid", "email", "profile", azureGraphURL.String()})
+		p.MicrosoftGraphAccessToken = result.AccessToken
+		p.MicrosoftGraphAccessTokenExpiresOn = &result.ExpiresOn
+
+	} else if p.MicrosoftGraphAccessTokenExpiresOn != nil && !p.MicrosoftGraphAccessTokenExpiresOn.IsZero() && p.MicrosoftGraphAccessTokenExpiresOn.Before(time.Now()) {
+		result, err = app.AcquireTokenSilent(ctx, []string{azureGraphURL.String()})
+		if err != nil {
+			result, err = app.AcquireTokenByCredential(ctx, []string{azureGraphURL.String()})
+		}
+		p.MicrosoftGraphAccessToken = result.AccessToken
+		p.MicrosoftGraphAccessTokenExpiresOn = &result.ExpiresOn
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *AzureProvider) getGroupsFromMicrosoftGraphAPI(ctx context.Context, s *sessions.SessionState) ([]string, error) {
+
+	if err := p.acquireMicrosoftGraphToken(ctx); err != nil {
+		return nil, err
+	}
+
+	var groupURL = getMicrosoftGraphURL(s.Email).String()
+	var groups []string
+
+	for groupURL != "" {
+		// logger.Printf("Calling Group API: %s", groupURL)
+		jsonRequest, err := requests.New(groupURL).
+			WithContext(ctx).
+			WithHeaders(makeAzureHeader(p.MicrosoftGraphAccessToken)).
+			Do().
+			UnmarshalJSON()
+		if err != nil {
+			return nil, err
+		}
+		groupURL, err = jsonRequest.Get("@odata.nextLink").String()
+		if err != nil {
+			groupURL = ""
+		}
+		groupsPage, err := getGroupsFromJSON(jsonRequest, p.GroupField)
+		if err != nil {
+			return nil, err
+		}
+		groups = append(groups, groupsPage...)
+	}
+
+	return groups, nil
+}
+
+func getGroupsFromJSON(json *simplejson.Json, groupField string) ([]string, error) {
+	var groups []string
+	for _, doc := range json.Get("value").MustArray() {
+		for k, v := range doc.(map[string]interface{}) {
+			if k == groupField {
+				groups = append(groups, v.(string))
+			}
+		}
+	}
+	return groups, nil
+}
+
+func (p *AzureProvider) getEmailFromProfileAPI(ctx context.Context, accessToken string) (string, error) {
+	if accessToken == "" {
+		return "", fmt.Errorf("missing access token")
+	}
+
+	json, err := requests.New(p.ProfileURL.String()).
+		WithContext(ctx).
+		WithHeaders(makeAzureHeader(accessToken)).
+		Do().
+		UnmarshalJSON()
+	if err != nil {
+		return "", err
+	}
+
+	email, err := getEmailFromJSON(json)
+	if email == "" && err == nil {
+		err = fmt.Errorf("empty email address: %v", err)
+	}
+	return email, err
 }
 
 func getEmailFromJSON(json *simplejson.Json) (string, error) {
@@ -336,23 +495,6 @@ func getEmailFromJSON(json *simplejson.Json) (string, error) {
 	}
 
 	return email, err
-}
-
-func (p *AzureProvider) getEmailFromProfileAPI(ctx context.Context, accessToken string) (string, error) {
-	if accessToken == "" {
-		return "", errors.New("missing access token")
-	}
-
-	json, err := requests.New(p.ProfileURL.String()).
-		WithContext(ctx).
-		WithHeaders(makeAzureHeader(accessToken)).
-		Do().
-		UnmarshalJSON()
-	if err != nil {
-		return "", err
-	}
-
-	return getEmailFromJSON(json)
 }
 
 // ValidateSession validates the AccessToken
